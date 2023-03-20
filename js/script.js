39a2d664f53547fbc0c0e3c93496fbfb8a0a8bbb
mcspe <mcspezzaferro@gmail.com>
2023-03-20 16:08:06 +0100

    [add] styled result printed on page

--- js/script.js
+++ js/script.js
@@ -1,24 +1,28 @@
-const tripKM = parseFloat(prompt('quanti km hai intenzione di percorrere? (inserire un valore numerico)'));
+const userName = prompt('Benvenuto! Per iniziare inserisci il tuo nome:');
 
-const userAge = parseInt(prompt('quanti anni hai? (inserire un valore numerico)'));
+const tripKM = parseFloat(prompt('Quanti km hai intenzione di percorrere? (inserire un valore numerico)')); //chiede all'utente il numero di km che dovrà percorrere
 
-const priceFull = 0.21;
+const userAge = parseInt(prompt('Quanti anni hai? (inserire un valore numerico)')); //chiede all'utente l'età
 
-//const priceExample = Math.round(0.3654 * 100) / 100;
+const priceFull = 0.21; // prezzo unitario per km
 
-const priceUnderAge = priceFull - ((priceFull * 20) / 100);
+//const priceExample = Math.round(0.3654 * 100) / 100; //arrotonda il numero moltiplicato per 100 cosìcche dividendolo in seguito per 100 sarà arrotondato al centesimo
 
-const priceOverAge = priceFull - ((priceFull * 40) / 100);
+const priceUnderAge = priceFull - ((priceFull * 20) / 100); // calcola il prezzo scontato del 20% per minorenni senza arrotondare
 
-let ticketPrice;
+const priceOverAge = priceFull - ((priceFull * 40) / 100); // calcola il prezzo scontato del 40% per ultrea sessantacinquenni senza arrotondare
 
 console.log(`
-  lunghezza viaggio: ${tripKM}
-  età passeggero: ${userAge}
-  prezzo intero: €${priceFull}
-  prezzo under 18: €${priceUnderAge}
-  prezzo over 65: €${priceOverAge}
+lunghezza viaggio: ${tripKM}
+età passeggero: ${userAge}
+prezzo intero: €${priceFull}
+prezzo under 18: €${priceUnderAge}
+prezzo over 65: €${priceOverAge}
 `);
+
+let ticketPrice = 0;
+
+//console.log(typeof(ticketPrice));
 
 if (userAge < 18){
   ticketPrice = tripKM * priceUnderAge;
@@ -26,10 +30,34 @@ if (userAge < 18){
   ticketPrice = tripKM * priceOverAge;
 } else{
   ticketPrice = tripKM * priceFull;
-}
+} // stabilisce il prezzo del biglietto in base all'età fornita dall'utente
 
-ticketPrice = Math.round((ticketPrice + Number.EPSILON)*100) / 100;
+ticketPrice = Math.round((ticketPrice + Number.EPSILON)*100) / 100; // arrotonda il prezzo finale al centesimo
 
-console.log(`
-  prezzo del biglietto: ${ticketPrice}
-`);
+console.log(`prezzo del biglietto: ${ticketPrice}`);
+
+document.getElementById('result').outerHTML = `
+  <div class="container text-center my-5 py-5 bg-danger bg-opacity-50 rounded-5">
+
+    <h1 class="mb-5">CALCOLA IL PREZZO DEL TUO BIGLIETTO</h1>
+
+    <p class="fs-4 fst-italic">
+      Ciao <span class="fw-bold">${userName}</span> in base alla tua età(<span class="fw-bold">${userAge}</span>) e ai km da percorrere(<span class="fw-bold">${tripKM}</span>) il prezzo del tuo biglietto sarà di:
+    </p>
+
+    <p class="fs-3 fw-bold mb-5">€ ${ticketPrice}*</p>
+
+    <div class="small fst-italic">
+
+      <p>
+        *Il prezzo del biglietto viene calcolato arrotondando alla seconda cifra decimale.
+        <ul class="list-unstyled">
+          <li>Il prezzo unitario per km è di €${priceFull}</li>
+          <li>Il prezzo per i minori di 18 anni è di €${priceUnderAge}, calcolato applicando uno sconto del 20% sul prezzo unitario</li>
+          <li>Il prezzo per colore che hanno più di 65 anni è di €${priceOverAge}, calcolato applicando uno sconto del 40% sul prezzo unitario</li>
+        </ul>        
+      </p>
+      
+    </div>
+  </div>
+`;
